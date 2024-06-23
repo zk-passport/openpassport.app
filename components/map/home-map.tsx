bcb8751e7da3d527c8f3732ec39f291a54177261
Vignesh-PS <selvavignesh128@gmail.com>
2024-06-24 00:35:20 +0530

    Highlighted supported, work in-progress countries

--- components/map/home-map.tsx
+++ components/map/home-map.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { Tooltip, Zoom } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Tooltip, Zoom } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import {
   ComposableMap,
   Geographies,
   Geography,
   Graticule,
   Sphere,
-} from 'react-simple-maps';
-import { createRoot } from 'react-dom/client';
+} from "react-simple-maps";
+import { createRoot } from "react-dom/client";
 
-const geoUrl = 'data/countries-110m.json';
+const geoUrl = "data/countries-110m.json";
 
 export default function MapChart() {
   enum issuePassTypes {
@@ -19,19 +19,13 @@ export default function MapChart() {
     ISSUE_WITHOUT_SUPPORT = 1001,
     ISSUE_WITH_SUPPORT = 1002,
   }
-  const encryptionNames = {
-    'sha1WithRSAEncryption': 'RSA with SHA-1',
-    'sha256WithRSAEncryption': 'RSA with SHA-256',
-    'sha512WithRSAEncryption': 'RSA with SHA-512',
-    'rsassaPss        ': 'RSA-PSS with SHA-256',
-    'ecdsa-with-SHA1': 'ECDSA with SHA-1',
-    'ecdsa-with-SHA256': 'ECDSA with SHA-256',
-    'ecdsa-with-SHA384': 'ECDSA with SHA-384',
-    'ecdsa-with-SHA512': 'ECDSA with SHA-512',
+  const SUPPORTED_ALGORITHMS = {
+    "RSA with SHA256": true,
   };
+
   const [selectedCountryInfo, setSelectedCountryInfo] = useState([]);
   const [allCountriesData, setAllCountriesData] = useState({});
-  const [selectedCountryName, setSelectedCountryName] = useState('');
+  const [selectedCountryName, setSelectedCountryName] = useState("");
   const [allIssuesCountry, setAllIssuesCountry] = useState({});
   const size = useWindowSize();
   const [isMobileView, setIsMobileView] = useState(false);
@@ -47,93 +41,106 @@ export default function MapChart() {
     }
   };
 
-  const formatJsonData = (dscInput: any = {}, cscaInput: any = {}, countryNames: any = {}): any => {
+  const formatJsonData = (
+    dscInput: any = {},
+    cscaInput: any = {},
+    countryNames: any = {}
+  ): any => {
     delete dscInput?.default;
     delete cscaInput?.default;
-   
+
     const countryData = {};
 
-    for(const countryCode of Object.keys(countryNames)){
+    for (const countryCode of Object.keys(countryNames)) {
       const name = countryNames[countryCode];
       countryData[name] = false;
 
       // verify specific country having dsc / csca records
       const dscCountryData = dscInput[countryCode];
       const cscaCountryData = cscaInput[countryCode];
-      if(dscCountryData?.length || cscaCountryData?.length){
+      if (dscCountryData?.length || cscaCountryData?.length) {
         // countryData[name] = {};
         const countryObj = {};
 
-        countryObj['name'] = name;
-        countryObj['countryCode'] = countryCode;
-        countryObj['dscExist'] = false;
-        countryObj['cscaExist'] = false;
-        
-        if(dscCountryData?.length > 0){
-          countryObj['dscExist'] = true;
-          countryObj['dscRecords'] = [...dscCountryData];
-          let count = 0;
+        countryObj["name"] = name;
+        countryObj["countryCode"] = countryCode;
+        countryObj["dscExist"] = false;
+        countryObj["cscaExist"] = false;
+
+        let count = 0;
+        if (dscCountryData?.length > 0) {
+          countryObj["dscExist"] = true;
+          countryObj["dscRecords"] = [...dscCountryData];
           const signatureCountryAlgs = {};
-          for(const dscEachAlg of dscCountryData){
+          for (const dscEachAlg of dscCountryData) {
             count += dscEachAlg?.amount;
             const signatureStr = `${dscEachAlg.signature_algorithm.toUpperCase()} with ${dscEachAlg.hash_algorithm.toUpperCase()}`;
             signatureCountryAlgs[signatureStr] = true;
           }
-          countryObj['amount'] = count;
-          countryObj['dscAlgs'] = Object.keys(signatureCountryAlgs);
+          countryObj["dscAlgs"] = Object.keys(signatureCountryAlgs);
         }
 
-        if(cscaCountryData?.length > 0){
-          countryObj['cscaExist'] = true;
-          countryObj['cscaRecords'] = [...cscaCountryData];
+        if (cscaCountryData?.length > 0) {
+          countryObj["cscaExist"] = true;
+          countryObj["cscaRecords"] = [...cscaCountryData];
+          for (const cscaEachAlg of cscaCountryData) {
+            count += cscaEachAlg?.amount;
+          }
         }
+        countryObj["amount"] = count;
 
         countryData[name] = countryObj;
-
       }
-
     }
-
 
     return countryData;
   };
 
-
   const fetchJsonInfo = async () => {
     try {
       // Intermediate Certificates (DSC) issued by each country
-      const dscFetchData = await fetch('https://raw.githubusercontent.com/zk-passport/proof-of-passport/dev/registry/outputs/dsc_formatted.json');
+      const dscFetchData = await fetch(
+        "https://raw.githubusercontent.com/zk-passport/proof-of-passport/dev/registry/outputs/dsc_formatted.json"
+      );
       const dscData = await dscFetchData.json();
-      
+
       // Top-level Certificates (CSCA) issued by each country
-      const cscaFetchData = await fetch('https://raw.githubusercontent.com/zk-passport/proof-of-passport/dev/registry/outputs/csca_formatted.json');
+      const cscaFetchData = await fetch(
+        "https://raw.githubusercontent.com/zk-passport/proof-of-passport/dev/registry/outputs/csca_formatted.json"
+      );
       const cscaData = await cscaFetchData.json();
-      
-      const countryNames = await import('./../../public/data/all-countries.json');
+
+      const countryNames = await import(
+        "./../../public/data/all-countries.json"
+      );
 
       if (!dscData || !cscaData) {
         return;
       }
 
-      const allCountriesData = formatJsonData({ ...dscData }, {...cscaData}, countryNames);
-      console.log('allCountriesData :>> ', allCountriesData);
+      const allCountriesData = formatJsonData(
+        { ...dscData },
+        { ...cscaData },
+        countryNames
+      );
+      console.log("allCountriesData :>> ", allCountriesData);
       setAllCountriesData(allCountriesData);
 
       // e-passport supported countries
       let ePassSupportCountries: any = await import(
-        './../../public/data/supported-countries.json'
+        "./../../public/data/supported-countries.json"
       );
       if (ePassSupportCountries?.default?.length) {
         ePassSupportCountries = ePassSupportCountries.default;
       }
 
-      // setIssuesSupportsVisuals(
-      //   allCountriesData,
-      //   ePassSupportCountries,
-      //   countryNames
-      // );
+      setIssuesSupportsVisuals(
+        allCountriesData,
+        ePassSupportCountries,
+        countryNames
+      );
     } catch (err) {
-      console.log('err :>> ', err);
+      console.log("err :>> ", err);
     }
   };
 
@@ -145,26 +152,37 @@ export default function MapChart() {
     if (!countryCertsData || !ePassCountries) {
       return;
     }
+
+    // construct obj to find the e-passport supported countries
+    const supportedCountriesObj = {};
+    ePassCountries.forEach((countryCode: string) => {
+      supportedCountriesObj[countryCode] = true;
+    });
+
     const countryRes = {};
-    for (const country of ePassCountries) {
+    for (const country of Object.keys(countryNames)) {
       const countryName = countryNames[country];
-      const supportedAlgs = countryCertsData[countryName];
+      const countryData = countryCertsData[countryName];
 
       countryRes[countryName] = {
         name: countryName,
-        issueType: issuePassTypes.ISSUE_WITHOUT_SUPPORT,
-        defaultColor: '#70ac48',
+        issueType: issuePassTypes.DO_NOT_ISSUE,
+        defaultColor: "#b0bfa7",
       };
 
-      if (supportedAlgs?.length) {
-        for (const alg of supportedAlgs) {
-          if (
-            // alg?.ENCRYPTION_CODE === 'sha1WithRSAEncryption' ||
-            alg?.ENCRYPTION_CODE === 'sha256WithRSAEncryption'
-          ) {
+      if (supportedCountriesObj[country]) {
+        countryRes[countryName].defaultColor = "#70ac48";
+        countryRes[countryName].issueType =
+          issuePassTypes.ISSUE_WITHOUT_SUPPORT;
+      }
+
+      // verified the dsc records are exists in supported algorithms or not
+      if (countryData?.dscAlgs?.length) {
+        for (const alg of countryData.dscAlgs) {
+          if (SUPPORTED_ALGORITHMS[alg]) {
             countryRes[countryName].issueType =
               issuePassTypes.ISSUE_WITH_SUPPORT;
-            countryRes[countryName].defaultColor = '#548233';
+            countryRes[countryName].defaultColor = "#548233";
           }
         }
       }
@@ -177,10 +195,10 @@ export default function MapChart() {
     fetchJsonInfo();
 
     // apply custom styles to map page
-    document.body.classList.add('globalMap');
+    document.body.classList.add("globalMap");
     // Clean up: Remove classes from body
     return () => {
-      document.body.classList.remove('globalMap');
+      document.body.classList.remove("globalMap");
     };
   }, []);
 
@@ -191,7 +209,7 @@ export default function MapChart() {
 
   const highLightInfo = (countryDscs: any = []) => {
     let info: React.JSX.Element;
-    return '';
+    return "";
     // if (countryDscs?.length > 0) {
     //   info = (
     //     <div className="highlightInfo">
@@ -264,8 +282,8 @@ export default function MapChart() {
         <Sphere
           stroke="#fff"
           strokeWidth={0.1}
-          id={'sphereline'}
-          fill={'#ffffff00'}
+          id={"sphereline"}
+          fill={"#ffffff00"}
         />
         <Geographies
           geography={geoUrl}
@@ -278,11 +296,11 @@ export default function MapChart() {
               <Tooltip
                 enterTouchDelay={0}
                 leaveTouchDelay={6000}
-                classes={{ tooltip: 'country-tooltip' }}
+                classes={{ tooltip: "country-tooltip" }}
                 title={highLightInfo(selectedCountryInfo)}
                 disableTouchListener={isMobileView}
                 disableHoverListener={isMobileView}
-                placement={'right'}
+                placement={"right"}
                 arrow
                 key={geo.rsmKey}
                 TransitionComponent={Zoom}
@@ -311,17 +329,17 @@ export default function MapChart() {
                       fill: allIssuesCountry[`${geo.properties.name}`]
                         ? allIssuesCountry[`${geo.properties.name}`]
                             .defaultColor
-                        : '#b0bfa7',
+                        : "#b0bfa7",
                     },
                     hover: {
                       fill: allIssuesCountry[`${geo.properties.name}`]
-                        ? '#4d7332'
-                        : '#b0bfa7',
+                        ? "#4d7332"
+                        : "#b0bfa7",
                     },
                     pressed: {
                       fill: allIssuesCountry[`${geo.properties.name}`]
-                        ? '#507f3a'
-                        : '#b0bfa7',
+                        ? "#507f3a"
+                        : "#b0bfa7",
                     },
                   }}
                 />
@@ -355,15 +373,15 @@ function useWindowSize() {
     }
 
     // Only execute on the client-side
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       // Add event listener
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
       // Call handler right away so state gets updated with initial window size
       handleResize();
 
       // Remove event listener on cleanup
-      return () => window.removeEventListener('resize', handleResize);
+      return () => window.removeEventListener("resize", handleResize);
     }
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
