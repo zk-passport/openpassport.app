0fa8a7ce0e5e959519e0408bf2212d883ff8410f
Vignesh-PS <selvavignesh128@gmail.com>
2024-06-26 02:18:17 +0530

    Visualized tooltip in devmode

--- components/map/home-map.tsx
+++ components/map/home-map.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Tooltip, Zoom } from '@mui/material';
+import { FormControlLabel, Grid, Switch, Tooltip, Zoom } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import {
   ComposableMap,
@@ -10,6 +10,7 @@ import {
   Sphere,
 } from 'react-simple-maps';
 import { createRoot } from 'react-dom/client';
+import ReactCountryFlag from 'react-country-flag';
 
 const geoUrl = 'data/countries-110m.json';
 
@@ -28,6 +29,7 @@ export default function MapChart() {
   const [allIssuesCountry, setAllIssuesCountry] = useState({});
   const size = useWindowSize();
   const [isMobileView, setIsMobileView] = useState(false);
+  const [devMode, setDevMode] = useState(false);
 
   const formatJsonData = (
     dscInput: any = {},
@@ -62,7 +64,7 @@ export default function MapChart() {
           const signatureCountryAlgs = {};
           for (const dscEachAlg of dscCountryData) {
             count += dscEachAlg?.amount;
-            if(dscEachAlg.signature_algorithm === 'rsapss'){
+            if (dscEachAlg.signature_algorithm === 'rsapss') {
               dscEachAlg.signature_algorithm = 'rsa-pss';
             }
             const signatureStr = `${dscEachAlg.signature_algorithm.toUpperCase()} with ${dscEachAlg.hash_algorithm.toUpperCase()}`;
@@ -159,6 +161,7 @@ export default function MapChart() {
         name: countryName,
         issueType: issuePassTypes.DO_NOT_ISSUE,
         defaultColor: '#b0bfa7',
+        countryCode: country,
       };
 
       if (supportedCountriesObj[country]) {
@@ -206,35 +209,6 @@ export default function MapChart() {
     setIsMobileView(isMobile);
   }, [size]);
 
-  //   {
-  //     "name": "Morocco",
-  //     "countryCode": "MA",
-  //     "dscExist": true,
-  //     "cscaExist": true,
-  //     "dscRecords": [
-  //         {
-  //             "signature_algorithm": "rsa",
-  //             "hash_algorithm": "sha256",
-  //             "curve_exponent": "65537",
-  //             "bit_length": 2048,
-  //             "amount": 3
-  //         }
-  //     ],
-  //     "dscAlgs": [
-  //         "RSA with SHA256"
-  //     ],
-  //     "cscaRecords": [
-  //         {
-  //             "signature_algorithm": "rsa",
-  //             "hash_algorithm": "sha256",
-  //             "curve_exponent": "65537",
-  //             "bit_length": 4096,
-  //             "amount": 5
-  //         }
-  //     ],
-  //     "amount": 8
-  // }
-
   const highLightInfo = (countryName: string) => {
     let info: React.JSX.Element;
     const countryData = allCountriesData[countryName];
@@ -242,8 +216,20 @@ export default function MapChart() {
       info = (
         <div className="highlightInfo">
           <h3 className="flex items-center">
-            <b>{countryName || ''} </b>
-          </h3><br />
+            <b>
+              {countryName || ''}{' '}
+              <ReactCountryFlag
+                countryCode={countryData.countryCode}
+                svg
+                style={{
+                  width: '2em',
+                  height: '1em',
+                }}
+                title={countryData.name}
+              />{' '}
+            </b>
+          </h3>
+          <br />
           <div className="issued-dscs">
             {countryData?.amount && (
               <p>
@@ -257,8 +243,9 @@ export default function MapChart() {
               </p>
             )}
             <br />
-            {countryData?.dscAlgs && <p>Signature algorithms:</p>}
-            {countryData?.dscAlgs?.length > 0
+            {/* tooltip data in normal mode */}
+            {countryData?.dscExist && !devMode && <p>Signature algorithms:</p>}
+            {countryData?.dscExist && !devMode
               ? countryData?.dscAlgs.map((dsc) => {
                   return (
                     <p key={dsc} className="flex items-center text-nowrap">
@@ -270,14 +257,81 @@ export default function MapChart() {
                   );
                 })
               : null}
+
+            {/* tooltip data in dev mode */}
+            {countryData?.cscaExist && devMode && (
+              <div>
+                <p>Top-level Certificates (CSCA)</p>
+                {countryData?.cscaExist && devMode
+                  ? countryData?.cscaRecords.map((csca) => {
+                      if (csca.signature_algorithm === 'rsapss') {
+                        csca.signature_algorithm = 'rsa-pss';
+                      }
+                      const signatureStr = `${csca.signature_algorithm.toUpperCase()} with ${csca.hash_algorithm.toUpperCase()}`;
+
+                      return (
+                        <p
+                          key={Math.random()}
+                          className="flex items-center text-nowrap"
+                        >
+                          &emsp;&emsp;&emsp;&nbsp;-&nbsp;
+                          {`${csca?.amount} issued with ${signatureStr}, exponent ${csca?.curve_exponent}, ${csca?.bit_length} bits`}
+                          <span>
+                            &nbsp;
+                            {SUPPORTED_ALGORITHMS[signatureStr] ? '✅' : '🚧'}
+                          </span>
+                        </p>
+                      );
+                    })
+                  : null}
+              </div>
+            )}
+            {countryData?.dscAlgs && devMode && (
+              <div>
+                <p>Intermediate Certificates (DSC)</p>
+                {countryData?.dscRecords?.length > 0 && devMode
+                  ? countryData?.dscRecords.map((dsc) => {
+                      if (dsc.signature_algorithm === 'rsapss') {
+                        dsc.signature_algorithm = 'rsa-pss';
+                      }
+                      const signatureStr = `${dsc.signature_algorithm.toUpperCase()} with ${dsc.hash_algorithm.toUpperCase()}`;
+
+                      return (
+                        <p
+                          key={Math.random()}
+                          className="flex items-center text-nowrap"
+                        >
+                          &emsp;&emsp;&emsp;&nbsp;-&nbsp;
+                          {`${dsc?.amount} issued with ${signatureStr}, exponent ${dsc?.curve_exponent}, ${dsc?.bit_length} bits`}
+                          <span>
+                            &nbsp;
+                            {SUPPORTED_ALGORITHMS[signatureStr] ? '✅' : '🚧'}
+                          </span>
+                        </p>
+                      );
+                    })
+                  : null}
+              </div>
+            )}
           </div>
         </div>
       );
     } else {
+      // const countryCode = getKeyByValue(all)
       info = (
         <div className="workInProgress">
           <h3 className="flex items-center">
             <b>{countryName || ''}</b>
+            &nbsp;
+            <ReactCountryFlag
+              countryCode={allIssuesCountry[countryName]?.countryCode}
+              svg
+              style={{
+                width: '2em',
+                height: '1em',
+              }}
+              title={countryName}
+            />{' '}
             &nbsp;
             {allIssuesCountry[`${countryName}`] ? '🚧' : null}
           </h3>
@@ -307,82 +361,134 @@ export default function MapChart() {
     return info;
   };
 
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
   return (
-    <div data-tip="" className="globalMap">
-      <ComposableMap
-        projectionConfig={{ center: isMobileView ? [10, 0] : [25, 10] }}
-        width={isMobileView ? 750 : 880}
-        height={500}
-      >
-        <Graticule stroke="#999" strokeWidth={0.2} />
-        <Sphere
-          stroke="#fff"
-          strokeWidth={0.1}
-          id={'sphereline'}
-          fill={'#ffffff00'}
-        />
-        <Geographies
-          geography={geoUrl}
-          fill="#e1e1e1"
-          stroke="#fff"
-          strokeWidth={0.3}
-        >
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Tooltip
-                enterTouchDelay={0}
-                leaveTouchDelay={6000}
-                classes={{ tooltip: 'country-tooltip' }}
-                title={highLightInfo(geo.properties.name)}
-                disableTouchListener={isMobileView}
-                disableHoverListener={isMobileView}
-                placement={'right'}
-                arrow
-                key={geo.rsmKey}
-                TransitionComponent={Zoom}
-                TransitionProps={{ timeout: 50 }}
+    <>
+      <div className={`mapRow`}>
+        <div className={`mapSection`}>
+          <div data-tip="" className="globalMap">
+            <ComposableMap
+              projectionConfig={{ center: isMobileView ? [10, 0] : [25, 10] }}
+              width={isMobileView ? 750 : 880}
+              height={500}
+            >
+              <Graticule stroke="#999" strokeWidth={0.2} />
+              <Sphere
+                stroke="#fff"
+                strokeWidth={0.1}
+                id={'sphereline'}
+                fill={'#ffffff00'}
+              />
+              <Geographies
+                geography={geoUrl}
+                fill="#e1e1e1"
+                stroke="#fff"
+                strokeWidth={0.3}
               >
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={() => {
-                    if (!isMobileView) {
-                      return;
-                    }
-                    if (isMobileView) {
-                      highLightInfo(geo.properties.name);
-                    }
+                {({ geographies }) =>
+                  geographies.map((geo) => (
+                    <Tooltip
+                      enterTouchDelay={0}
+                      leaveTouchDelay={6000}
+                      classes={{ tooltip: 'country-tooltip' }}
+                      title={highLightInfo(geo.properties.name)}
+                      disableTouchListener={isMobileView}
+                      disableHoverListener={isMobileView}
+                      placement={'right'}
+                      arrow
+                      key={geo.rsmKey}
+                      TransitionComponent={Zoom}
+                      TransitionProps={{ timeout: 50 }}
+                    >
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        onClick={() => {
+                          if (!isMobileView) {
+                            return;
+                          }
+                          if (isMobileView) {
+                            highLightInfo(geo.properties.name);
+                          }
+                        }}
+                        onMouseEnter={() => {
+                          if (isMobileView) {
+                            highLightInfo(geo.properties.name);
+                          }
+                        }}
+                        style={{
+                          default: {
+                            fill: allIssuesCountry[`${geo.properties.name}`]
+                              ? allIssuesCountry[`${geo.properties.name}`]
+                                  .defaultColor
+                              : '#b0bfa7',
+                          },
+                          hover: {
+                            fill: allIssuesCountry[`${geo.properties.name}`]
+                              ? '#4d7332'
+                              : '#b0bfa7',
+                          },
+                          pressed: {
+                            fill: allIssuesCountry[`${geo.properties.name}`]
+                              ? '#507f3a'
+                              : '#b0bfa7',
+                          },
+                        }}
+                      />
+                    </Tooltip>
+                  ))
+                }
+              </Geographies>
+            </ComposableMap>
+          </div>
+        </div>
+      </div>
+      <Grid container spacing={2} className="sm:mt-3 mt-0 countryDetailsRow">
+        <Grid
+          sm={6}
+          xs={12}
+          id="countryDetails"
+          className="md:hidden text-gray-900 sm:border-0"
+        >
+          &nbsp;
+        </Grid>
+        <Grid
+          sm={6}
+          xs={12}
+          className="legend-info lg:right-3 text-black relative bottom-2 lg:bottom-7 lg:absolute"
+        >
+          <h2 className={`homeTitle`}>Proof of Passport country coverage</h2>
+          <div className="legend-info-item flex items-center">
+            <p className="w-8 h-4 bg-[#548233] me-2"></p> Supported countries
+          </div>
+          <div className="legend-info-item flex items-center">
+            <p className="w-8 h-4 bg-[#70ac48] me-2"></p> Work in progress
+          </div>
+          <div className="legend-info-item flex items-center">
+            <p className="w-8 h-4 bg-[#b0bfa7] me-2"></p> Not issuing e-passport
+          </div>
+          <div className="legend-info-item flex items-center">
+            <FormControlLabel
+              control={
+                <Switch
+                  color="success"
+                  checked={devMode}
+                  onChange={() => {
+                    console.log('toggled');
+                    devMode ? setDevMode(false) : setDevMode(true);
                   }}
-                  onMouseEnter={() => {
-                    if (isMobileView) {
-                      highLightInfo(geo.properties.name);
-                    }
-                  }}
-                  style={{
-                    default: {
-                      fill: allIssuesCountry[`${geo.properties.name}`]
-                        ? allIssuesCountry[`${geo.properties.name}`]
-                            .defaultColor
-                        : '#b0bfa7',
-                    },
-                    hover: {
-                      fill: allIssuesCountry[`${geo.properties.name}`]
-                        ? '#4d7332'
-                        : '#b0bfa7',
-                    },
-                    pressed: {
-                      fill: allIssuesCountry[`${geo.properties.name}`]
-                        ? '#507f3a'
-                        : '#b0bfa7',
-                    },
-                  }}
+                  name="devMode"
                 />
-              </Tooltip>
-            ))
-          }
-        </Geographies>
-      </ComposableMap>
-    </div>
+              }
+              label="Dev Mode"
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
