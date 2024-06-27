a4fdab66a925ebab1545ba4e73776e5c4dfbbcba
0xturboblitz <florent.tavernier@gmail.com>
2024-06-27 14:51:00 -0700

    map subdomain redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'map.proofofpassport.com',
+            },
+          ],
+          destination: '/map/:path*',
+        },
+      ],
+      // afterFiles: [
+      //     {
+      //         source: '/map/_next/:path*',
+      //         destination: '/_next/:path*',
+      //     },
+      //     {
+      //         source: '/map/static/:path*',
+      //         destination: '/static/:path*',
+      //     },
+      //     {
+      //         source: '/map/data/:path*',
+      //         destination: '/data/:path*',
+      //     },
+      //     {
+      //         source: '/map/favicon.ico',
+      //         destination: '/favicon.ico',
+      //     },
+      // ],
+    };
+  },
+};
+
+module.exports = nextConfig;
