8fe3f359468264254dea23f82b323df46fecba66
turnoffthiscomputer <colin.remi07@gmail.com>
2024-08-30 14:45:24 +0200

    add callback to playground

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -4,6 +4,12 @@ import React, { useEffect, useRef, useState } from 'react';
 import { countryCodes, OpenPassportVerifierReport } from '@proofofpassport/sdk';
 import { Autocomplete, TextField } from '@mui/material';
 import { OpenPassportQRcode } from '@proofofpassport/sdk';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { tokyoNightDay } from '@uiw/codemirror-theme-tokyo-night-day';
+import { whiteLight } from '@uiw/codemirror-theme-white'
 
 const countryOptions = Object.values(countryCodes);
 
@@ -23,6 +29,9 @@ function Showcase() {
     const [age, setAge] = useState('');
     const [error, setError] = useState(false);
     const [helperText, setHelperText] = useState('');
+    const [callback, setCallback] = useState(`(appName, toast) => {
+  toast(\`Congrats, you have been added to the group \${appName}\`);
+}`);
 
     const scope = '@OpenPassportPlayground';
     const userID = crypto.randomUUID();
@@ -62,13 +71,38 @@ function Showcase() {
 
     const handleSuccessfulVerification = (verificationResult: OpenPassportVerifierReport) => {
         console.log('Proof verified successfully:', verificationResult);
+        try {
+            // Execute the callback code
+            const callbackFunction = eval(`(${callback})`);
+            callbackFunction(appName, toast);
+        } catch (error) {
+            console.error('Error executing callback:', error);
+            toast.error('Error executing callback');
+        }
     };
 
+    const [qrCodeSize, setQrCodeSize] = useState(300);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setQrCodeSize(window.innerWidth > 1024 ? 400 : 300);
+        };
+
+        // Set initial size
+        handleResize();
+
+        // Add event listener
+        window.addEventListener('resize', handleResize);
+
+        // Clean up
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <div className="App flex flex-col items-center my-16">
-            <div className="p-8 flex flex-col items-center">
-                <h1 className="text-4xl font-bold mb-24 text-black">OpenPassport playground</h1>
-                <div className="mb-16">
+        <div className="App flex flex-col items-center my-8 px-4 md:my-32  md:mb-48 mb-8">
+            <h1 className="text-3xl md:text-4xl font-bold mb-8 md:mb-12 text-black text-center">OpenPassport playground</h1>
+            <div className="flex flex-col md:flex-row w-full max-w-6xl md:mt-12">
+                <div className="w-full md:w-1/2 md:pr-4 flex items-center justify-center mb-8 md:mb-0 ">
                     <OpenPassportQRcode
                         appName={appName}
                         scope={scope}
@@ -79,35 +113,64 @@ function Showcase() {
                         ]}
                         onSuccess={handleSuccessfulVerification}
                         devMode={true}
+                        size={qrCodeSize}
                     />
                 </div>
-                <TextField
-                    label="Name of the app"
-                    variant="outlined"
-                    value={appName}
-                    onChange={(event) => setAppName(event.target.value)}
-                    error={error}
-                    sx={{ width: '300px', marginBottom: '24px' }}
-                />
-                <TextField
-                    label="Older than"
-                    variant="outlined"
-                    value={age}
-                    onChange={handleOlderThanFieldChange}
-                    error={error}
-                    helperText={helperText}
-                    sx={{ width: '300px', marginBottom: '24px' }}
-                />
-                <Autocomplete
-                    disablePortal
-                    id="nationality-select"
-                    options={countryOptions}
-                    sx={{ width: 300, marginBottom: '24px' }}
-                    renderInput={(params) => <TextField {...params} label="Nationality" />}
-                    value={nationality}
-                    onChange={(event, newValue) => setNationality(newValue || '')}
-                />
+                <div className="w-full md:w-1/2 md:pl-4 flex flex-col justify-between mt-6">
+                    <div className="flex flex-col justify-between mb-8">
+                        <TextField
+                            label="Name of the app"
+                            variant="outlined"
+                            value={appName}
+                            onChange={(event) => setAppName(event.target.value)}
+                            error={error}
+                            sx={{ width: '100%', marginBottom: '12px' }}
+                        />
+                        <TextField
+                            label="Older than"
+                            variant="outlined"
+                            value={age}
+                            onChange={handleOlderThanFieldChange}
+                            error={error}
+                            helperText={helperText}
+                            sx={{ width: '100%', marginBottom: '12px' }}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="nationality-select"
+                            options={countryOptions}
+                            sx={{ width: '100%' }}
+                            renderInput={(params) => <TextField {...params} label="Nationality" />}
+                            value={nationality}
+                            onChange={(event, newValue) => setNationality(newValue || '')}
+                        />
+                    </div>
+                    <div>
+                        <h1 className="text-xs text-[#666666] ml-2 mb-1" style={{ fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif' }}>Callback function</h1>
+                        <div className="code-editor-container" style={{
+                            width: '100%',
+                            borderRadius: '6px',
+                            overflow: 'hidden',
+                            border: '1px solid var(--border-color, #c4c4c4)',
+                            transition: 'border-color 0.2s ease',
+                        }}>
+                            <CodeMirror
+                                value={callback}
+                                maxHeight='130px'
+                                theme={whiteLight}
+                                extensions={[javascript({ jsx: true })]}
+                                onChange={(value) => setCallback(value)}
+                                basicSetup={{
+                                    lineNumbers: false,
+                                    foldGutter: false,
+                                }}
+                                className="custom-codemirror"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }
